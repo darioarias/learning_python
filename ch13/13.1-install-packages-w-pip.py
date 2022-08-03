4757fc4a37ee30eec8144e287c6ff1c083fc9e8b
Dario <darioarias12@gmail.com>
2022-08-03 15:10:08 -0400

    helpful notes form chapter 13

---
+++ ch13/13.1-install-packages-w-pip.py
@@ -0,0 +1,13 @@
+
+
+
+""" Installing specific Package Versions With Version specifiers """
+
+# Installing the latest version of requests whose version number is 2 or greater
+# pip install requests>=2.0
+
+
+# version specifiers
+# <=, >=  Inclusive less than and greater than specifiers
+# <, >  Exclusive less than and greater than specifiers
+# ==   Exactly equal to specifier
