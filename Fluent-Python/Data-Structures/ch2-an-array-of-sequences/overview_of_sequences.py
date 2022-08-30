de31a23003ce0984937bc86c6d35794568c055c2
Dario <darioarias12@gmail.com>
2022-08-30 08:59:04 -0400

    update to ch2 which makes it easier to nav and search

---
+++ Fluent-Python/Data-Structures/ch2-an-array-of-sequences/overview_of_sequences.py
@@ -0,0 +1,7 @@
+## Overview of Built-In Sequences
+# Container sequences
+  # Can hold items of different types, including nested containers. Some examples:
+  # list, tuple, and collections.deque
+  
+# Flat sequences
+  # Hold items of one simple type. Some examples: str, bytes, and array.array.
