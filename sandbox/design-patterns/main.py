bfbf3d447e4534edf30650dbe0ae8aa3b72c9647
Dario <darioarias12@gmail.com>
2022-09-13 17:45:47 -0400

    testing the flyweight pattern

--- sandbox/design-patterns/main.py
+++ sandbox/design-patterns/main.py
@@ -1,6 +1,9 @@
-from strategy_pattern import Flies
-from strategy_pattern.Animal import Animal
-from strategy_pattern.animals import Dog, Duck
+# from strategy_pattern import Flies
+# from strategy_pattern.Animal import Animal
+# from strategy_pattern.animals import Dog, Duck
+
+from flightweight_pattern.Rectangle import Rectangle
+
 
 # state = Flies.CantFly()
 
@@ -10,8 +13,19 @@ from strategy_pattern.animals import Dog, Duck
 # animal.fly = Flies.CanFly()
 
 # print(animal.fly.fly())
+rec1 = Rectangle((1,2,3), (0, 0))
+rec2 = Rectangle((1,2,3), (4, 0))
 
-pet = Dog('Tiguere')
-pet2 = Duck('Goose')
+print(rec2.factory)
+# pet = Dog('Tiguere')
+# pet2 = Duck('Goose')
 
-print(pet2.flies())
+# print(pet2.flies())
+
+# factory = RectangleFactory()
+# factory.getColor((255, 255, 255))
+# factory.getColor((255, 150, 255))
+# factory.getColor((255, 255, 150))
+# factory.getColor((255, 255, 150, .05))
+
+# print(factory)
