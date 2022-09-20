938df1b2ff5a1c52bbb2630a526360293c5f6c38
Dario <darioarias12@gmail.com>
2022-09-20 12:39:45 -0400

    updated aliases to provide support for actions that are frequently used.

--- sandbox/aliases.zsh
+++ sandbox/aliases.zsh
@@ -1,14 +1,5 @@
 #! /bin/bash
 
-_args_exits() {
-  # echo "arg $1"
-  if [ -z $1 ]; then
-    return -1
-  else 
-    return 0
-  fi
-}
-
 create_code() {
   if [ -z $1 ]; then
     echo "You must provide a file to create and open"
@@ -40,18 +31,31 @@ create_commit() {
   return $?
 }
 
-create_commit_code() {
+new_commit_code() {
+  COMMIT_MSG="$2"
   if [ -z $1 ]; then
-    echo "You must provide a file to create and add to git"
+    echo "You must provide a file to create and add to git">&1
     return -1
   elif [ -z $2 ]; then
-    echo "You did not provide a message for git commit"
-    return -1
+    echo "You did not provide a message for git commit">&1
+    COMMIT_MSG="Create $1"
   fi
   
-  echo $1 | xargs -I % sh -c "{ touch % && git add % && git commit -m '$2' && code $1 && nodemon $1; }"
+  echo $1 | xargs -I % sh -c "{ touch % && git add % && git commit -m '$COMMIT_MSG' && code $1 && nodemon $1; }"
   # echo "git commit -m '$2'"
   return $?
+}
+
+add_commit() {
+  COMMIT_MSG="$2"
+  if [ -z $1 ]; then
+    echo "You must provide a file to create and add to git">&1
+    return -1
+  elif [ -z $2 ]; then
+    echo "You did not provide a message for git commit">&1
+    COMMIT_MSG="update to $1"
+  fi 
+  echo $1 | xargs -I % sh -c "{ git add % && git commit -m '$COMMIT_MSG'; }"
 }
 
 watch() {
