f854c073d19ab3ba031fa7c15c70e39ca67ea0f9
Dario <darioarias12@gmail.com>
2022-10-12 16:02:37 -0400

    book keeping

--- Fluent-Python/Classes-and-Protocals/overloaded_signatures.py
+++
@@ -1,7 +0,0 @@
-# some times, you have to provide type hints to your functions
-# for this we use stub files (.pyi). When a function supports
-# more than one signature, you can use @overload decorator
-# look at program.py and program.pyi
-from program import sum_concat
-
-# print(sum_concat(char for char in ('a', 'b', 'c', 'd')))
--- Fluent-Python/Classes-and-Protocals/program.pyi
+++
@@ -1,7 +0,0 @@
-from collections.abc import Iterable
-from typing import overload
-
-@overload
-def sum_concat(__itr: Iterable[int]) -> int: ...
-@overload
-def sum_concat(__itr: Iterable[str]) -> str: ...
