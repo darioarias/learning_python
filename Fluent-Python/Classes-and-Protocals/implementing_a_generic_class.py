50e6d0d8c9c1f4bdcd5f13dbc95bda0d9bca808f
Dario <darioarias12@gmail.com>
2022-10-12 12:01:34 -0400

    implementing a generic class.

---
+++ Fluent-Python/Classes-and-Protocals/implementing_a_generic_class.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import abc
+import random
+from collections.abc import Iterable
+from typing import Generic, TypeVar
+
+
+class Tombola(abc.ABC):
+    @abc.abstractmethod
+    def load(self, iterable):
+        """Add items from an iterable."""
+
+    @abc.abstractmethod
+    def pick(self):
+        """Remove item at random, returning it.
+
+        This method should raise `LookupError` when the instance is empty.
+        """
+
+    def loaded(self):
+        """Return `True` if there's at least 1 item, `False` otherwise."""
+        return bool(self.inspect())
+
+    def inspect(self):
+        """Return a sorted tuple with the items currently inside."""
+        items = []
+        while True:
+            try:
+                items.append(self.pick())
+            except LookupError:
+                break
+        self.load(items)
+        return tuple(items)
+
+
+T = TypeVar("T")
+
+
+class LottoBlower(Tombola, Generic[T]):
+    def __init__(self, items: Iterable[T]) -> None:
+        self._balls = list[T](items)
+
+    def load(self, items: Iterable[T]) -> None:
+        self._balls.extend(items)
+
+    def pick(self) -> T:
+        try:
+            position = random.randrange(len(self._balls))
+        except ValueError:
+            raise LookupError("Pick from empty LottoBlower")
+        return self._balls.pop(position)
+
+    def loaded(self) -> bool:
+        return bool(self._balls)
+
+    def inspect(self) -> tuple[T, ...]:
+        return tuple(self._balls)
