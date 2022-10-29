b04d291d8072f51e90be39b451e249564adead44
Dario <darioarias12@gmail.com>
2022-10-29 12:12:20 -0400

    looking at how to build a lazy class

---
+++ Fluent-Python/Control-Flow/ch17-iterators-generators-and-classic-coroutines/lazy_sentences.py
@@ -0,0 +1,19 @@
+
+
+# building a Sentence class lazily to save memory 
+import re
+import reprlib
+
+RE_WORD = re.compile(r'\w+')
+
+class Sentence:
+
+    def __init__(self, text) -> None:
+        self.text = text 
+    
+    def __repr__(self) -> str:
+        return f"Sentence({reprlib.repr(self.text)})"
+    
+    def __iter__(self):
+        for match in RE_WORD.finditer(self.text):
+            yield match.group()
