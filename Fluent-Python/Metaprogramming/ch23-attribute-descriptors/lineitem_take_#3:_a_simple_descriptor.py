1dbd79cf2ce75a84819c0e3a3f82ad63e1d944f5
Dario <darioarias12@gmail.com>
2023-01-26 07:16:00 -0500

    Quantity descriptors manage attributes in LineItem

--- Fluent-Python/Metaprogramming/ch23-attribute-descriptors/lineitem_take_#3:_a_simple_descriptor.py
+++ Fluent-Python/Metaprogramming/ch23-attribute-descriptors/lineitem_take_#3:_a_simple_descriptor.py
@@ -30,15 +30,34 @@ class Quantity:
     def __init__(self, storage_name: str) -> None:
         self.storage_name = storage_name
 
-    def __set__(self, instance: object, value: int) -> None:
+    def __set__(self, instance: object, value: float) -> None:
         if value > 0:
             instance.__dict__[self.storage_name] = value
         else:
             msg = f"{self.storage_name} must be > 0"
             raise ValueError(msg)
 
-    def __get__(self, instance: object, owner: object) -> Quantity | int:
+    def __get__(self, instance: object, owner: object) -> float | Quantity:
         if instance is None:
             return self
         else:
             return instance.__dict__[self.storage_name]
+
+
+class LineItem:
+    weight = Quantity("weight")
+    price = Quantity("price")
+
+    def __init__(self, description: str, weight: float, price: float) -> None:
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self) -> float:
+        assert isinstance(self.weight, float)
+        assert isinstance(self.price, float)
+
+        return self.weight * self.price
+
+
+# item = LineItem("White truffle", 1, 1)
