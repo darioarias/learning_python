17a4435fac7f6dc970d0b19af332699f26c14089
Dario <darioarias12@gmail.com>
2023-01-26 06:42:31 -0500

    Terms to understand descriptors

---
+++ Fluent-Python/Metaprogramming/ch23-attribute-descriptors/lineitem_take_#3:_a_simple_descriptor.py
@@ -0,0 +1,22 @@
+# Descriptor class
+## A class implementing the descriptor protocol.
+
+# Managed class
+## The class where the descriptor instances are
+## declared as class attributes.
+
+# Descriptor instance
+## Each instance of a descriptor class,
+## declared as a class attribute of the managed class.
+
+# Managed instance
+## One instance of the managed class.
+
+# Storage attribute
+## An attribute of the managed instance that holds
+## the value of a managed attribute for that particular instance.
+
+
+# Managed attribute
+## A public attribute in the managed class that is handled by
+## a descriptor instance, with values stored in storage attributes.
