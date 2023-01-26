96c4351f0763a154db39f45946cf3e6a1ca49fca
Dario <darioarias12@gmail.com>
2023-01-26 13:28:20 -0500

    Quantity and NonBlank, concrete Validated subclasses

--- Fluent-Python/Metaprogramming/ch23-attribute-descriptors/lineitem_take_#5:_a_new_descriptor_type.py
+++ Fluent-Python/Metaprogramming/ch23-attribute-descriptors/lineitem_take_#5:_a_new_descriptor_type.py
@@ -12,3 +12,22 @@ class Validated(abc.ABC):
     @abc.abstractmethod
     def validate(self, name: str, value: float | str) -> float | str:
         """return validated value or raise ValueError"""
+
+
+class Quantity(Validated):
+    """a number greater than zero"""
+
+    def validate(self, name: str, value: float) -> float:
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")
+        return value
+
+
+class NonBlank(Validated):
+    """a string with at least one non-space character"""
+
+    def validate(self, name: str, value: str) -> str:
+        value = value.strip()
+        if not value:
+            raise ValueError(f"{name} cannot be blank")
+        return value
